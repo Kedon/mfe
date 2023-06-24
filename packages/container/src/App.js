33096eb40d9f244e87aebcb014ffe6fd9d4b6dc0
Kedon <valdenir@kedon.com.br>
2023-06-24 09:00:05 -0300

    Ajuste

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>Hi there! How are you?</h1>
+        <h1>Hi there! How are you? Welcome!</h1>
         <hr />
         <MarketingApp />
         </div>
