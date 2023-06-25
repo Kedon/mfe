519b964b8c6e3e02c7871c037582ea980705fda0
Kedon <valdenir@kedon.com.br>
2023-06-25 10:52:51 -0300

    Novas implementações

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,22 +1,33 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
-import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom";
-import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Header from './components/Header';
+
+const LazyMarketingApp = lazy(() => import('./components/MarketingApp'));
+const LazyAuthApp = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: "co"
-})
+  productionPrefix: 'co',
+});
 
 export default () => {
-    return (
-        <BrowserRouter>
-            <StylesProvider generateClassName={generateClassName} >
-                <div>
-                    <Header />
-                    <MarketingApp />
-                </div>
-            </StylesProvider>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/auth" component={LazyAuthApp} />
+              <Route path="/" component={LazyMarketingApp} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 };
